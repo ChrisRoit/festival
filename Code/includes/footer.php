a841b01ec943ecbdc99821bf70c12e78ba64c66c
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-09-30 11:01:28 +0200

    pauze push

---
+++ Code/includes/footer.php
@@ -0,0 +1,8 @@
+<footer>
+    <div class="footer-wrapper">
+        <ul id="footer-bar">
+            <li class="footer-item"><i class="fas fa-mail" /> test</li>
+            <i class="fas fa-envelope-square"></i>
+        </ul>
+    </div>
+</footer>
--- Code/index.php
+++ Code/index.php
@@ -7,8 +7,13 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/footer.css" />
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     </head>
     <body>
         <?php include "includes/navbar.php"; ?>
+
+    
+        <?php include "includes/footer.php"; ?>
     </body>
 </html>
