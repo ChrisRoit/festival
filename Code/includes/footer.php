ef85b0350ed9277059cdbc3c60963860f91435bc
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-09-30 12:14:11 +0200

    pauze

--- Code/includes/footer.php
+++ Code/includes/footer.php
@@ -1,8 +1,9 @@
 <footer>
-    <div class="footer-wrapper">
+    <div id="footer-wrapper">
         <ul id="footer-bar">
-            <li class="footer-item"><i class="fas fa-mail" /> test</li>
-            <i class="fas fa-envelope-square"></i>
+            <li class="footer-item"><p><i class="fas fa-envelope-square"></i> E-Mail</p></li>
+            <li class="footer-item"><p> <i class="fas fa-phone"></i>  Telefoon</p></li>   
         </ul>
+        <div id="maker_name">Vul een naam in</div>
     </div>
 </footer>
