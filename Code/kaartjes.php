1edb7d97cb578b9b6fce88155011acff9246ff77
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-09-30 14:37:28 +0200

    laatste commit navbar en footer zijn voor nu af(de basis) kan altijd anders gestyled worden of anders ingedeeld.

--- Code/kaartjes.php
+++ Code/kaartjes.php
@@ -7,9 +7,45 @@
         <title>Document</title>
         <link rel="stylesheet" href="css/footer.css" />
         <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/kaartjes.css" />
     </head>
     <body>
-        <?php include "includes/navbar.php"; ?>
+        <header>
+            <?php include "includes/navbar.php"; ?>
+        </header>
+        <main>
+            <section id="formulier-wrapper">
+                <form action="#" method="POST">
+                    <label>Naam</label>
+                    <div>
+                        <input type="text" name="name" required/>
+                    </div>              
+                    <label>Adres</label>
+                    <div>
+                        <input type="text" name="address" required />
+                    </div>
+                    <label>Email</label>
+                    <div>
+                        <input type="email" name="email" required />
+                    </div>
+                    <label>Aantal Kaartjes</label>
+                    <div>
+                        <input type="number" name="amount" reqruired />
+                    </div>
+                    <div>
+                        <?php 
+                            $choices = array("vrijdag","zaterdag","beide");
+                            foreach($choices as $choice){
+                                echo "<input type='radio' name='day' value='$choice'>" . ucfirst($choice) . "</input>";
+                            }
+                        ?>
+                    </div>
+                    <div>
+                        <button type="submit">Koop</button>
+                    </div>
+                </form>
+            </section>
+        </main>
         
 
         <?php include "includes/footer.php" ?>
