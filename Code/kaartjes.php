f42fa44612adce988e9b5f044950e3c762569b08
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-10-06 10:37:42 +0200

    temp push. Moeten aan een nieuw project werken.

--- Code/kaartjes.php
+++ Code/kaartjes.php
@@ -15,40 +15,40 @@
         </header>
         <main id="main-content">
             <div class="jumbotron-wrapper">
-            <div class="custom-jumbotron">
-                <section id="formulier-wrapper">
-                    <form action="#" method="POST">
-                        <label>Naam</label>
-                        <div class="input-wrapper">
-                            <input class="standard-input" type="text" name="name" required/>
-                        </div>              
-                        <label>Adres</label>
-                        <div class="input-wrapper">
-                            <input class="standard-input" type="text" name="address" required />
-                        </div>
-                        <label>Email</label>
-                        <div class="input-wrapper">
-                            <input class="standard-input" type="email" name="email" required />
-                        </div>
-                        <label>Aantal Kaartjes</label>
-                        <div class="input-wrapper">
-                            <input class="standard-input" type="number" min=1 name="amount" reqruired />
-                        </div>
-                        <div class="input-wrapper">
-                            <?php 
-                                $choices = array("vrijdag","zaterdag","beide");
-                                foreach($choices as $choice){
-                                    echo "<input type='radio' name='day' value='$choice'>" . ucfirst($choice) . "</input>";
-                                }
-                            ?>
-                        </div>
-                        <div>
-                            <button class="custom-button" type="submit">Koop</button>
-                        </div>
-                    </form>
-                </section>
-            </div>
+                <div class="custom-jumbotron">
+                    <section id="formulier-wrapper">
+                        <form action="#" method="POST">
+                            <label>Naam</label>
+                            <div class="input-wrapper">
+                                <input class="standard-input" type="text" name="name" required/>
+                            </div>              
+                            <label>Adres</label>
+                            <div class="input-wrapper">
+                                <input class="standard-input" type="text" name="address" required />
                             </div>
+                            <label>Email</label>
+                            <div class="input-wrapper">
+                                <input class="standard-input" type="email" name="email" required />
+                            </div>
+                            <label>Aantal Kaartjes</label>
+                            <div class="input-wrapper">
+                                <input class="standard-input" type="number" min=1 name="amount" reqruired />
+                            </div>
+                            <div class="input-wrapper">
+                                <?php 
+                                    $choices = array("vrijdag","zaterdag","beide");
+                                    foreach($choices as $choice){
+                                        echo "<input type='radio' name='day' value='$choice'>" . ucfirst($choice) . "</input>";
+                                    }
+                                ?>
+                            </div>
+                            <div>
+                                <button class="custom-button" type="submit">Koop</button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
+            </div>
         </main>
         
 
