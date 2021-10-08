b3d1152d27d4fa244839220c68812954747f68e2
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-10-08 20:51:02 +0200

    pauze push, moet even weg. animatie is bijna klaar.

--- Code/js/scroll-detect.js
+++ Code/js/scroll-detect.js
@@ -2,13 +2,19 @@
 //voeg een event listener toe die kijkt of de gebruiker scrollt
 document.addEventListener("scroll",function(e){
     //pak de bovenste navbar element
-    navbar = document.getElementById("main-navbar-wrapper");
+    navbar = document.getElementById("navbar-background-color");
     //als de scrollY positie niet op 0 staat en de class main-navbar-wrapper-transparent niet in het boven gepakte navbar element zit.
     if(window.scrollY != 0 && navbar.classList.contains("main-navbar-wrapper-transparent") == false){
+        //
+        navbar.classList.remove("main-navbar-wrapper-not-transparent");
+        console.log(1);
         //voeg de main-navbar-wrapper-transparent class toe aan de navbar zodat de css toegevoegd word die de navbar 100% transparant maakt.
         navbar.classList.add("main-navbar-wrapper-transparent");
     //als de scrollY positie 0 is dus de gebruiker tot boven aan de pagina heeft gescrollt. verwijder dan de main-navbar-wrapper-transparent class zodat het neon effect van de navbar weer zichtbaar is en de navbar niet meer 100% transparant is.
     }else if(window.scrollY == 0){
         navbar.classList.remove("main-navbar-wrapper-transparent");
+        console.log(2);
+        //
+        navbar.classList.add("main-navbar-wrapper-not-transparent");
     }
 });
