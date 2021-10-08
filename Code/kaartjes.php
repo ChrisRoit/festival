5af6995294832575dc6eb36bb12d3ab4acdefc13
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-10-09 00:52:01 +0200

    footer afgemaakt

--- Code/kaartjes.php
+++ Code/kaartjes.php
@@ -9,6 +9,7 @@
         <link rel="stylesheet" href="css/footer.css" />
         <link rel="stylesheet" href="css/style.css" />
         <link rel="stylesheet" href="css/kaartjes.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <script src="js/scroll-detect.js"></script>
     </head>
     <body>
