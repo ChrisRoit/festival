2a49aa591ebbeb50602af20ff9bd99ebc9378b3d
SamaelAlderson <166225@student.horizoncollege.nl>
2021-10-09 02:28:36 +0200

    requirements complete
    requirements complete

--- Code/index.php
+++ Code/index.php
@@ -8,25 +8,29 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="css/style.css" />
         <link rel="stylesheet" href="css/footer.css" />
+        <link rel="stylesheet" href="css/navbar.css">
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     </head>
     <body>
         <?php include "includes/navbar.php"; ?>
-        <section>
+        <section id="s1">
             <div>
-                <img src="" alt="">
+                <img src="images/Cirque-Magique.jpeg" alt="haha boy" class="img">
             </div>
+
             <div>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, quaerat architecto optio placeat at delectus atque nobis soluta, voluptatem voluptates reprehenderit? Eveniet a quasi alias!</p>
-            </div>
+                <h1>Lorem, ipsum.</h1>
+                <p id="p1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nobis itaque neque, eveniet doloribus mollitia harum ab necessitatibus est esse maiores, velit doloremque, ipsum iste?</p>
+            </div>   
         </section>
-        
-        <section>
+
+        <section id="s2">
             <div>
+                <h1>Lorem, ipsum.</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nulla ipsum magnam, omnis ea qui? Quidem libero itaque magnam autem. Vero ipsum voluptatum aspernatur magnam.</p>
             </div>
             <div>
-                <img src="" alt="">
+                <img src="images/canal-boat.png" alt="boat time lol exdee">
             </div>
         </section>
     
