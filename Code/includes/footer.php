c60eb1c39ad2297982b31bcd1fe65d8f4634444c
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-10-09 13:55:29 +0200

    footer text kleur gefixed die brak schijnbaar op bepaalde paginas?

--- Code/includes/footer.php
+++ Code/includes/footer.php
@@ -1,8 +1,8 @@
 <footer>
     <div id="footer-wrapper">
-        <div id="email"><i class="fa fa-envelope"></i> downthesnakehole@snake.com</div>
+        <div id="email" class="footer-text"><i class="fa fa-envelope"></i> downthesnakehole@snake.com</div>
         
-        <div id="maker_name">Luuk/Samael/Chris</div>
-        <div id="phone"><i class="fa fa-phone"></i> +31 625473800</div>
+        <div id="maker_name" class="footer-text">Luuk/Samael/Chris</div>
+        <div id="phone" class="footer-text"><i class="fa fa-phone"></i> +31 625473800</div>
     </div>
 </footer>
