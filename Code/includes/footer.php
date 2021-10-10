047b4be8ec5a8904855d0f5a9934f876156c8cce
Chris <89969353+ChrisRoit@users.noreply.github.com>
2021-10-10 17:56:41 +0200

    De informatie pagina is af

--- Code/includes/footer.php
+++ Code/includes/footer.php
@@ -2,7 +2,7 @@
     <div id="footer-wrapper">
         <div id="email" class="footer-text"><i class="fa fa-envelope"></i> downthesnakehole@snake.com</div>
         
-        <div id="maker_name" class="footer-text">Luuk/Samael/Chris</div>
+        <div id="maker_name" class="footer-text">Chris</div>
         <div id="phone" class="footer-text"><i class="fa fa-phone"></i> +31 625473800</div>
     </div>
 </footer>
